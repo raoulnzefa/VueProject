aef707b03e0b2ef7ca858625b3a23451c873fb94
Satyam Negi <satyanegi.negi@gmail.com>
2021-10-05 16:35:28 -0400

    Login page commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,74 @@
 <template>
-  <div class="container-fluid">
-    <h1>Login to Project1 Page</h1>
-    <br />
-    <br />
-    <div class="row">
-      <div class="col-md-3"></div>
-      <div class="col-md-6">
-        <form>
-          <div class="form-group">
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <button type="submit" class="btn btn-primary">Login</button>
+    <div class="bg">
+        <h2>sdjasd</h2>
+        <h4>Login here for the entire new world of shopping..</h4>
+        <form @submit.prevent="submitAction">
+            <div class="form-group" style="margin-top: 50px; margin-left: 20px; text-align: left; vertical-align: middle">
+                <label for="userName">Username</label>
+                <input type="text" style="margin-top: 10px; width:300px; margin-left: 10px; display:inline-block;" v-model="userName" name="userName" class="form-control" :class="{ 'is-invalid': submit && !userName }" />
+                <div v-show="submit && !userName" class="invalid-feedback">UserName is mandatory</div>
+            </div>
+            <div class="form-group" style="margin-top: 20px; margin-left: 20px; text-align: left;">
+                <label htmlFor="password">Password</label>
+                <input type="password" style="margin-top: 10px; margin-left: 15px; width:300px; display:inline-block;" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submit && !password }" />
+                <div v-show="submit && !password" class="invalid-feedback">Password is mandatory</div>
+            </div>
+            <div class="form-group" style="margin-top: 30px; margin-left: 150px;">
+                <button class="btn btn-primary" v-on:click="onClickEvent()" >Login</button>
+                <router-link to="/register" class="btn btn-link" style="color: white">Register</router-link>
+            </div>
         </form>
-      </div>
-      <div class="col-md-3"></div>
     </div>
-  </div>
 </template>
-<script>
-export default {};
-</script>
 
-<style scoped></style>
+<script>
+// eslint-disable-next-line no-unused-vars
+import { mapState, mapActions } from 'vuex'
+export default {
+    data () {
+        return {
+            userName: '',
+            password: '',
+            submit: false
+        }
+    },
+    computed: {
+        ...mapState('userAcc', ['status'])
+    },
+    methods: {
+        ...mapActions('userAcc', ['login', 'logout']),
+        submitAction (e) {
+            this.submit = true;
+            const { userName, password } = this;
+            if (userName && password) {
+                this.login({ userName, password })
+            }
+        },
+        onClickEvent(){
+            window.location.href = "/";
+        }
+    }
+};
+</script>
+<style scoped>
+h2{
+  color: transparent;
+}
+h4{
+  color: white;
+  margin-top: 180px;
+}
+label{
+  color: white;
+}
+.bg{
+  text-align: left;
+  vertical-align: middle;
+  background-image: url("../assets/login.jpeg"); /* The image used */
+  background-color: #cccccc; /* Used if the image is unavailable */
+  height: 800px; /* You must set a specified height */
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover; /* Resize the background image to cover the entire container */
+}
+</style>
